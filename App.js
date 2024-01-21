e57140eb5771c466d3ebd33aacffe627a391958b
TZU-CHAO WANG <john29917958@gmail.com>
2024-01-21 16:50:34 +0800

    Add simple feature of changing app name

--- App.js
+++ App.js
@@ -1,20 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [name, setName] = useState("Hello Expo app");
+
+  handle = function () {
+    let randomName = getRandomString(5);
+    randomName = toFirstCharCapitalized(randomName);
+    setName(randomName);
+  };
+
+  function toFirstCharCapitalized(string) {
+    let firstChar = string[0];
+    firstChar = firstChar.toUpperCase();
+    let firstCharCapitalizedString = firstChar + string.slice(1);
+    return firstCharCapitalizedString;
+  }
+
+  function getRandomString(length) {
+    let result = "";
+    const characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Welcome to {name}</Text>
+      <Pressable style={styles.button} onPress={this.handle.bind(this)}>
+        <Text style={styles.buttonTextColor}>Click to change app name</Text>
+      </Pressable>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  title: {
+    marginBottom: 20,
+    fontSize: 30,
+    fontWeight: "100",
+  },
+
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 2,
+    backgroundColor: "#f44336",
+  },
+
+  buttonTextColor: {
+    color: "#ffffff",
   },
 });
